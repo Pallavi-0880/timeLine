53228e15021d2fe8d6f3c51774bfec191430b0e0
Pallavi Thakur <pallavithakur0880@gmail.com>
2024-01-03 16:20:48 +0530

    create timeline Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Time from "./Time.jsx"
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Time-Line</h1>
+      <br />
+      <Time year ={2003} event ="I was born"/>
+      <Time year ={2007} event ="In 2007, I had fallen and a stone had enter my chin, due to which I had to be beaten a lot by my mother."/>
+      <Time year ={2011} event ="In 2011, I fell off my bicycle and have never ridden a bicycle since."/>
+      <Time year ={2014} event ="My aunt died in 2014.I cried a lot at that time"/>
+      <Time year ={2018} event ="In 2018, I went to the Games for the first time and at that time I was very proud."/>
+      <Time year ={2020} event ="In 2020, when I went to Dharamshala for the Games and ran on the synthetic track there.  That was too barefoot and because of that I got blisters on my feet."/>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/Time.jsx
@@ -0,0 +1,9 @@
+const Time= ({year, event})=>{
+    return(
+        <div>
+        <h2>{year}</h2>
+        <p>{event}</p>
+        </div>
+    )
+}
+export default Time
